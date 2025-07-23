a4fb94d414395677619b39f3341189b31115ac04
CilleBorromeo <cilleanthony@gmail.com>
2025-07-23 15:42:09 +0800

    Add files via upload

---
+++ BORROMMEO_Exercise1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+	printf("----------------------------\n");
+	printf("Part 1. Basic Greetings\n\n");
+	printf("Hello!\n");
+	printf("Welcome to C programming.\n");
+	printf("\n----------------------------\n");
+	printf("Part 2. Escape Sequences\n\n");
+	printf("She said, \"C is fun!\"\n");
+	printf("\n----------------------------\n");
+	printf("Part 3. Tabs and Newlines\n\n");
+	printf("Menu:\n");
+	printf("\t1. Start\n");
+	printf("\t2. Settings\n");
+	printf("\t3. Exit\n");
+	printf("\n----------------------------\n");
+	printf("Part 4.  Alignment Challenge\n\n");
+	printf("Name\t Grade\n");
+	printf("Alice\t A\n");
+	printf("Bob\t B\n");
+	printf("Charlie\t A\n");
+	printf("\n----------------------------\n");
+	printf("Part 5. Console Art\n\n");
+	printf("+-----+\n");
+	printf("|     |\n");
+	printf("| BOX |\n");
+	printf("|     |\n");
+	printf("+-----+\n");
+	
+	return 0;
+}
