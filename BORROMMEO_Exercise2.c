6c4bed789eff1384f057e9b331843b42875923f6
CilleBorromeo <cilleanthony@gmail.com>
2025-07-25 14:43:39 +0800

    Add files via upload

---
+++ BORROMMEO_Exercise2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	printf("The sum of 7 + 9 is %d\n", 7 + 9);
+	printf("The value of 22 divided by 7 is approximately %.6f\n", 22.0 / 7);
+	printf("Rounded to two decimals places: %.2f\n", 22.0 / 7);
+	printf("The letter after 'Y' is %c\n",'Y'+ 1);
+	printf("The phrase %s has been printed\n", "\"C is fun!\"");
+	printf("The number 255 in hexadecimal is %x\n", 255);
+	printf("This is 100%% correct");
+
+	return 0;
+}
